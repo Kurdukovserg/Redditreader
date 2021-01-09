4556b924a889a84fa08137c6c5a68ecba4ca6450
Kurdukovserg <74448744+Kurdukovserg@users.noreply.github.com>
2021-01-09 17:15:54 +0200

    Initial commit
    Reading data from reddit/top and parsing.
    Now its just name of Author and date in UTC still lot of work

---
+++ app/src/main/java/com/example/redditreader/MainActivity.java
@@ -0,0 +1,109 @@
+package com.example.redditreader;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private String TAG = MainActivity.class.getSimpleName();
+    ListView listView;
+    ArrayList<HashMap<String, String>> contactList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.listView);
+        contactList = new ArrayList<>();
+        new GetRedditData().execute();
+    }
+
+    private class GetRedditData extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Toast.makeText(MainActivity.this,"Json Data is downloading",Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            HttpHandler sh = new HttpHandler();
+            // Making a request to url and getting response
+            String url = "https://www.reddit.com/r/popular/top.json";
+            String jsonStr = sh.makeServiceCall(url);
+            Log.e(TAG, "Response from url: " + jsonStr);
+            if (jsonStr != null) {
+                try {
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+                    JSONObject data = jsonObj.getJSONObject("data");
+                    JSONArray contacts = data.getJSONArray("children");
+
+                    // looping through All Contacts
+                    for (int i = 0; i < contacts.length(); i++) {
+                        JSONObject d = contacts.getJSONObject(i);
+                        JSONObject c = d.getJSONObject("data");
+                        String name = c.getString("author");
+                        String date = c.getString("created_utc");
+                        String comments = c.getString("num_comments");
+                        // tmp hash map for single contact
+                        HashMap<String, String> contact = new HashMap<>();
+                        // adding each child node to HashMap key => value
+                        contact.put("name", name);
+                        contact.put("date", date);
+                        contact.put("comments", comments);
+                        Log.e(TAG, "contact map: " + contact);
+                        contactList.add(contact);
+                    }
+                } catch (final JSONException e) {
+                    Log.e(TAG, "Json parsing error: " + e.getMessage());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),
+                                    "Json parsing error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+                }
+
+            } else {
+                Log.e(TAG, "Couldn't get json from server.");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),
+                                "Couldn't get json from server. Check LogCat for possible errors!",
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            ListAdapter adapter = new SimpleAdapter(MainActivity.this, contactList,
+                    R.layout.list_item, new String[]{ "name","date"},
+                    new int[]{R.id.author, R.id.date_of_creation});
+            listView.setAdapter(adapter);
+        }
+    }
+}
